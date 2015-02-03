89deabcd27f8eb9320017ec2fc831e253df64283
mikifus <mikifus@gmail.com>
2015-02-03 22:47:36 +0100

    Added support for multiple servers. Added servers array. Added default server as preference. Added a support and feedback text on the about page.

--- app/src/main/java/com/mikifus/padland/NewPadActivity.java
+++ app/src/main/java/com/mikifus/padland/NewPadActivity.java
@@ -1,11 +1,16 @@
 package com.mikifus.padland;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class NewPadActivity extends PadLandActivity {
 
@@ -14,32 +19,63 @@ public class NewPadActivity extends PadLandActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newpad);
 
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText textField = (EditText) findViewById(R.id.editText);
+        // Set default position choosen by user
+        // This can be changed on the preferences
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = (ArrayAdapter) spinner.getAdapter();
 
-                String padName = (String) textField.getText().toString();
-                String padServer = "https://pad.riseup.net/p/";
-                String padUrl = "https://pad.riseup.net/p/" + padName;
+        // Server list to provide a default value
+        String server_list [] = getResources().getStringArray(R.array.etherpad_servers_name);
 
-                if (padName == "") {
-                    return;
-                }
+        // Getting user preferences
+        Context context = getApplicationContext();
+        SharedPreferences userDetails = context.getSharedPreferences(getPackageName() + "_preferences", context.MODE_PRIVATE);
+        String default_server = userDetails.getString("padland_default_server", server_list[0]);
 
-                Intent padViewIntent =
-                        new Intent(NewPadActivity.this, PadViewActivity.class);
-                padViewIntent.putExtra("padName", padName);
-                padViewIntent.putExtra("padServer", padServer);
-                padViewIntent.putExtra("padUrl", padUrl);
-
-                startActivity(padViewIntent);
-            }
-        });
+        // We get position and set it as default
+        spinner.setSelection(adapter.getPosition(default_server));
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         return super.onCreateOptionsMenu(menu, R.menu.new_pad);
+    }
+
+    public void onCreateButtonClick(View w){
+
+        String padName = getPadNameFromInput((TextView) findViewById(R.id.editText));
+        String padPrefix = getPadPrefixFromSpinner((Spinner) findViewById(R.id.spinner));
+        String padServer = getPadServerFromSpinner((Spinner) findViewById(R.id.spinner));
+
+        String padUrl = padPrefix + padName;
+
+        if (padName == "") {
+            return;
+        }
+
+        Intent padViewIntent =
+                new Intent(NewPadActivity.this, PadViewActivity.class);
+        padViewIntent.putExtra("padName", padName);
+        padViewIntent.putExtra("padServer", padServer);
+        padViewIntent.putExtra("padUrl", padUrl);
+
+        startActivity(padViewIntent);
+    }
+
+    private String getPadNameFromInput(TextView input){
+        String padName = (String) input.getText().toString();
+
+        return padName;
+    }
+
+    private String getPadPrefixFromSpinner(Spinner spinner){
+        String padPrefix = getResources().getStringArray(R.array.etherpad_servers_url_padprefix)[spinner.getSelectedItemPosition()];
+
+        return padPrefix;
+    }
+
+    private String getPadServerFromSpinner(Spinner spinner){
+        String padServer = getResources().getStringArray(R.array.etherpad_servers_url_home)[spinner.getSelectedItemPosition()];
+
+        return padServer;
     }
 }
