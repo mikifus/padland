aa6309b77a95c04e04ca3e5c78a8ac62ff6308f7
mikifus <mikifus@gmail.com>
2015-05-13 20:01:54 +0200

    Fixed errors. Allowing to display pads in vertical orientation. Pads load better. Pad names must be valid to URL now.

--- app/src/main/java/com/mikifus/padland/PadViewActivity.java
+++ app/src/main/java/com/mikifus/padland/PadViewActivity.java
@@ -6,12 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
@@ -20,6 +22,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ShareActionProvider;
 import android.widget.Toast;
+import com.pnikosis.materialishprogress.ProgressWheel;
 
 import java.util.Date;
 
@@ -33,6 +36,11 @@ import java.util.Date;
 public class PadViewActivity extends PadLandDataActivity {
     private WebView webView;
     private String current_padUrl = "";
+
+    /*
+    Progress wheel is a fancy alternative to the ProgressBar, that wasn't working at all.
+     */
+    private ProgressWheel pwheel;
 
     /**
      * Local class to handle the Javascript onLoad callback
@@ -53,14 +61,11 @@ public class PadViewActivity extends PadLandDataActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
+        super.onCreate(savedInstanceState);
 
-        // ProgressBar
-        // WARNING: it does not work
-        this._loadProgressBar();
 
         // Forces landscape view
-        this.setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
+//        this.setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT );
 
         // If no network...
         if( !isNetworkAvailable() ) {
@@ -68,25 +73,40 @@ public class PadViewActivity extends PadLandDataActivity {
             return;
         }
 
-        setContentView( R.layout.activity_padview );
+        setContentView(R.layout.activity_padview);
+        _loadProgressWheel();
 
         this._makePadUrl();
         this._saveNewPad();
         this._updateViewedPad();
         this._makeWebView();
 
-        this.loadUrl( "file:///android_asset/PadView.html" );
+        this.loadUrl("file:///android_asset/PadView.html");
     }
 
     /**
-     * Loads the fancy progressbar in the top of the screen.
-     * I don't see it at all, but there is no error.
+     * Loads the fancy ProgressWheel to show it's loading.
      */
-    private void _loadProgressBar(){
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        requestWindowFeature(Window.FEATURE_PROGRESS);
-        setProgressBarIndeterminateVisibility(true);
-        setProgressBarVisibility(true);
+    private void _loadProgressWheel()
+    {
+        pwheel = (ProgressWheel) findViewById(R.id.progress_wheel);
+        pwheel.spin();
+    }
+
+    private void _setProgressWheelProgress( int progress )
+    {
+        if( progress < 25 )
+        {
+            // At least something visible
+            progress = 25;
+        }
+        float p = progress / 100;
+        Log.d("LOAD_PROGRESS_LOG", String.valueOf(progress));
+        pwheel.setVisibility(View.VISIBLE);
+        pwheel.setProgress(p);
+        if ( progress > 99 ) {
+            pwheel.setVisibility(View.GONE);
+        }
     }
 
     /**
@@ -108,8 +128,9 @@ public class PadViewActivity extends PadLandDataActivity {
         padData padData;
 
         if( pad_id > 0 ){
-            Cursor c = this._getPadDataById( pad_id );
+            Cursor c = this._getPadDataById(pad_id);
             padData = new padData( c );
+            c.close();
         }else{
             padData = this._getPadDataFromIntent();
         }
@@ -128,14 +149,14 @@ public class PadViewActivity extends PadLandDataActivity {
         padData padData;
 
         if ( Intent.ACTION_VIEW.equals( action ) ) {
-            String padUrl = String.valueOf( myIntent.getData() );
+            String padUrl = String.valueOf(myIntent.getData());
             padData = makePadData( null, null, padUrl );
         } else {
             String padName = myIntent.getStringExtra( "padName" );
             String padServer = myIntent.getStringExtra( "padServer" );
-            String padUrl = myIntent.getStringExtra( "padUrl" );
+            String padUrl = myIntent.getStringExtra("padUrl");
 
-            padData = makePadData( padName, padServer, padUrl );
+            padData = makePadData(padName, padServer, padUrl);
         }
 
         return padData;
@@ -159,7 +180,9 @@ public class PadViewActivity extends PadLandDataActivity {
 
         if(c != null && c.getCount() > 0) {
             c.moveToFirst();
-            return c.getLong( 0 );
+            long id = c.getLong(0);
+            c.close();
+            return id;
         }
 
         return 0;
@@ -181,7 +204,7 @@ public class PadViewActivity extends PadLandDataActivity {
 
             values.put( PadLandContentProvider.NAME, intentData.getName() );
             values.put( PadLandContentProvider.SERVER, intentData.getServer() );
-            values.put( PadLandContentProvider.URL, intentData.getUrl() );
+            values.put(PadLandContentProvider.URL, intentData.getUrl());
 
             result = savePadData( 0, values );
         }
@@ -198,7 +221,7 @@ public class PadViewActivity extends PadLandDataActivity {
         boolean result = false;
         long pad_id = this._getPadId();
         if ( pad_id != 0 ) {
-            accessUpdate( pad_id );
+            accessUpdate(pad_id);
             result = true;
         }
         return result;
@@ -226,20 +249,27 @@ public class PadViewActivity extends PadLandDataActivity {
 
                 webView.loadUrl("javascript:start('" + current_padUrl + "', '" + default_username + "', '" + default_color + "' )");
             }
+
+            /**
+             * This method is here because of http to https redirects.
+             * @param view
+             * @param url
+             * @return
+             */
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                return false;
+            }
         });
-        this._makeWebSettings( webView );
+        this._makeWebSettings(webView);
 
         webView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
-                setProgress(progress * 100);
-                if(progress > 99) {
-                    setProgressBarIndeterminateVisibility(false);
-                    setProgressBarVisibility(false);
-                }
+                _setProgressWheelProgress(progress);
             }
         });
 
-        this._addJavascriptOnLoad( webView );
+        this._addJavascriptOnLoad(webView);
 
         return webView;
     }
@@ -252,15 +282,19 @@ public class PadViewActivity extends PadLandDataActivity {
         // Enable Javascript
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setUseWideViewPort(true);
+        //webSettings.setUseWideViewPort(true);
         webSettings.setLoadWithOverviewMode(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(true);
+        //webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
     }
 
     /**
      * With a JavsascriptInterface I manage to call functions when the page loads
      * @param webView
      */
-    private void _addJavascriptOnLoad( WebView webView ){
+    private void _addJavascriptOnLoad( WebView webView) {
         webView.addJavascriptInterface(new JavascriptCallbackHandler(), "webviewScriptAPI");
         String fulljs = "javascript:(\n    function() { \n";
         fulljs += "        window.onload = function() {\n";
@@ -304,9 +338,10 @@ public class PadViewActivity extends PadLandDataActivity {
         // This creates a fake cursor
         MatrixCursor matrixCursor= new MatrixCursor(columns);
         startManagingCursor(matrixCursor);
-        matrixCursor.addRow(new Object[] { 0, padName, padServer, padUrl, 0, 0 });
+        matrixCursor.addRow(new Object[]{0, padName, padServer, padUrl, 0, 0, 0 });
 
         padData padData = new padData( matrixCursor );
+        matrixCursor.close();
 
         return padData;
     }
