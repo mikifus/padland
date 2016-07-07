8d6e75393da1f49231969bcacd6dc8e3c0d0c39f
mikifus <>
2016-07-08 01:31:28 +0200

    Added New server form dialog.

--- app/src/main/java/com/mikifus/padland/ServerListActivity.java
+++ app/src/main/java/com/mikifus/padland/ServerListActivity.java
@@ -1,10 +1,13 @@
 package com.mikifus.padland;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.mikifus.padland.Adapters.ServerListAdapter;
+import com.mikifus.padland.Dialog.NewServer;
 
 /**
  * Created by mikifus on 29/05/16.
@@ -13,13 +16,22 @@ public class ServerListActivity extends PadLandDataActivity {
 
     ArrayAdapter mAdapter;
 
+    private static final String NEW_SERVER_DIALOG = "dialog_new_server";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_list);
         mAdapter = new ServerListAdapter(ServerListActivity.this, android.R.layout.simple_list_item_2);
-        if (findViewById(R.id.listView) != null) {
-            ((ListView) findViewById(R.id.listView)).setAdapter(mAdapter);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        if (listView != null) {
+            listView.setAdapter(mAdapter);
         }
+    }
+
+    public void onNewServerClick(View view) {
+        FragmentManager fm = getSupportFragmentManager();
+        NewServer dialog = new NewServer();
+        dialog.show(fm, NEW_SERVER_DIALOG);
     }
 }
