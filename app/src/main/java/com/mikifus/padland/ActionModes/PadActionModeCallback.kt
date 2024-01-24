05d9f6a5fdd09ac8cb3cec16ce076e1b6a8bfaf2
mikifus <2385163+mikifus@users.noreply.github.com>
2024-01-24 01:05:00 +0100

    Separate action mode for each item type: pads and groups. Each SelectionTracker is coupled with an action mode so the selection doesn't mix. Drag and Drop result for groups is not defined.

---
+++ app/src/main/java/com/mikifus/padland/ActionModes/PadActionModeCallback.kt
@@ -0,0 +1,99 @@
+package com.mikifus.padland.ActionModes
+
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.view.ActionMode
+import com.mikifus.padland.Activities.PadListActivity
+import com.mikifus.padland.R
+
+class PadActionModeCallback(activity: PadListActivity): ActionMode.Callback {
+
+    var padActionMode: ActionMode? = null
+    var padListActivity: PadListActivity
+
+    init {
+        padListActivity = activity
+    }
+
+    /**
+     * Called when the action mode is created; startActionMode() was called
+     *
+     * @param mode
+     * @param menu
+     * @return boolean
+     */
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        // Inflate a menu resource providing context menu items
+        if(mode != null) {
+            val inflater = mode.menuInflater
+            inflater.inflate(R.menu.rowselection, menu)
+            padActionMode = mode
+        }
+        return true
+    }
+
+    /**
+     * Called each time the action mode is shown. Always called after onCreateActionMode, but
+     * may be called multiple times if the mode is invalidated.
+     *
+     * @param mode
+     * @param menu
+     * @return boolean
+     */
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        return false // Return false if nothing is done
+    }
+
+    /**
+     * Called when the user selects a contextual menu item
+     *
+     * @param mode
+     * @param item
+     * @return
+     */
+    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem): Boolean {
+        return when (item.itemId) {
+//            R.id.menuitem_group -> {
+//                menuGroup(checkedItemIds)
+//                // Action picked, so close the CAB
+//                mode.finish()
+//                true
+//            }
+//
+//            R.id.menuitem_copy -> {
+//                menuCopy(checkedItemIds)
+//                // Action picked, so close the CAB
+//                mode.finish()
+//                true
+//            }
+//
+//            R.id.menuitem_edit -> {
+//                menuEdit(checkedItemIds)
+//                // Action picked, so close the CAB
+//                mode.finish()
+//                true
+//            }
+//
+//            R.id.menuitem_delete -> {
+//                askDelete(checkedItemIds)
+//                // Action picked, so close the CAB
+//                mode.finish()
+//                true
+//            }
+//
+//            R.id.menuitem_share -> {
+//                menuShare(checkedItemIds)
+//                // Action picked, so close the CAB
+//                mode.finish()
+//                true
+//            }
+//
+            else -> false
+        }
+    }
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
+        padActionMode = null
+        padListActivity.clearPadSelection()
+    }
+}
