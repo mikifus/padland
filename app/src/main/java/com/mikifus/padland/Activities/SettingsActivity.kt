cb21266a88586eebb4f41730b70a3eaa1fa8e4b1
mikifus <2385163+mikifus@users.noreply.github.com>
2024-02-10 23:05:21 +0100

    Navigate back from SettingsActivity.

--- app/src/main/java/com/mikifus/padland/Activities/SettingsActivity.kt
+++ app/src/main/java/com/mikifus/padland/Activities/SettingsActivity.kt
@@ -3,7 +3,9 @@ package com.mikifus.padland.Activities
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import androidx.lifecycle.ViewModelProvider
 import androidx.preference.ListPreference
 import androidx.preference.Preference
@@ -98,5 +100,24 @@ class SettingsActivity : AppCompatActivity() {
             super.onConfigurationChanged(newConfig)
             (findPreference("padland_default_color") as ColorPickerListPreference?)?.reload()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                // TODO: If Settings has multiple levels, Up should navigate up
+                // that hierarchy.
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
