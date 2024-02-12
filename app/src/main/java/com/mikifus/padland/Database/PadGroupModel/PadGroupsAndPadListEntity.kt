ef2eaba8f4e3a383e917e84aad1d7e9056a292aa
mikifus <2385163+mikifus@users.noreply.github.com>
2024-02-12 14:01:44 +0100

    Reimplementing content provider.

--- app/src/main/java/com/mikifus/padland/Database/PadGroupModel/PadGroupsAndPadListEntity.kt
+++ app/src/main/java/com/mikifus/padland/Database/PadGroupModel/PadGroupsAndPadListEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.ForeignKey
 import com.mikifus.padland.Database.PadModel.Pad
 
 @Entity(
-    tableName = "padlist_padgroups",
+    tableName = PadGroupsAndPadList.TABLE_NAME,
     primaryKeys = ["_id_group", "_id_pad"],
     foreignKeys = [
         ForeignKey(
@@ -24,4 +24,8 @@ import com.mikifus.padland.Database.PadModel.Pad
 data class PadGroupsAndPadList (
     @ColumnInfo(name = "_id_group") val mGroupId: Long,
     @ColumnInfo(name = "_id_pad") val mPadId: Long,
-)
+) {
+    companion object {
+        const val TABLE_NAME = "padlist_padgroups"
+    }
+}
