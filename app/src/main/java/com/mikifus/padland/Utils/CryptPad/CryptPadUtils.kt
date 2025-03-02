d26dbfa2b6c2c5fecfd624d6b8f8503265bed467
mikifus <2385163+mikifus@users.noreply.github.com>
2025-03-02 01:13:45 +0100

    Add CryptPadUtils

---
+++ app/src/main/java/com/mikifus/padland/Utils/CryptPad/CryptPadUtils.kt
@@ -0,0 +1,35 @@
+package com.mikifus.padland.Utils.CryptPad
+
+class CryptPadUtils {
+    companion object {
+        private val CRYPTPAD_REPLACE_TYPE_NAME_REGEX = Regex("(\\d)/[a-z]+(/?)")
+        private const val CRYPTPAD_REPLACE_TYPE_NAME_REGEX_SUBSTITUTION = "$1/__type__$2"
+        private val CRYPTPAD_REPLACE_TYPE_PATH_REGEX = Regex("(/?)[a-z]+/#/(\\d)/[a-z]*(/?)")
+        private const val CRYPTPAD_REPLACE_TYPE_PATH_REGEX_SUBSTITUTION = "$1__type__/#/$2/__type__$3"
+
+        fun replaceCryptPadType(nameOrUrl: String, type: String): String {
+            var typeString = type
+            val typeFormatIndex = type.indexOf("/#")
+            if (typeFormatIndex > -1) {
+                typeString = typeString.substring(1, typeFormatIndex)
+            }
+            val isName = CRYPTPAD_REPLACE_TYPE_NAME_REGEX.containsMatchIn(nameOrUrl)
+            val regex = if (isName) CRYPTPAD_REPLACE_TYPE_NAME_REGEX else CRYPTPAD_REPLACE_TYPE_PATH_REGEX
+            val substitution = if (isName) CRYPTPAD_REPLACE_TYPE_NAME_REGEX_SUBSTITUTION else CRYPTPAD_REPLACE_TYPE_PATH_REGEX_SUBSTITUTION
+            return nameOrUrl.replace(regex, substitution.replace("__type__", typeString))
+        }
+
+        fun makePadPrefixFromServerAndType(server: String, type: String): String {
+            var serverString = server
+            var typeString = type
+            if (serverString.endsWith("/")) {
+                serverString = serverString.substringBeforeLast("/")
+            }
+            if (!typeString.endsWith("/#/")) {
+                typeString = "/$type/#/"
+
+            }
+            return serverString + typeString
+        }
+    }
+}
